f8fcc6799da27a00809ee8f3fc3e5a3aeda44c8d
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2025-11-09 12:47:02 +0500

    feat: secure training routes with admin token verification

--- server/src/routes/training.routes.js
+++ server/src/routes/training.routes.js
@@ -2,11 +2,14 @@ import express from "express";
 import {
   createTraining,
   getTraining,
+  getAllTrainings,
 } from "../controllers/training.controller.js";
+import { verifyAdminToken } from "../middlewares/index.js";
 
 const router = express.Router();
 
-router.post("/", createTraining);
-router.get("/:id", getTraining);
+router.post("/", verifyAdminToken, createTraining);
+router.get("/", verifyAdminToken, getAllTrainings);
+router.get("/:id", verifyAdminToken, getTraining);
 
 export default router;
