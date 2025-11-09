2720892eee32109c64e511beaf4206efd2bdd6f2
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2025-11-09 09:57:58 +0500

    feat: add employee parameter to feedback fetching for enhanced filtering

--- client/src/services/feedback.service.js
+++ client/src/services/feedback.service.js
@@ -5,14 +5,20 @@ import axiosInstance from "../axios/axiosInstance";
 // Fetch Feedbacks
 export const getFeedbacks = createAsyncThunk(
   "feedbacks/getFeedbacks",
-  async ({ review, page = 1, limit = 10 }, { rejectWithValue }) => {
+  async (
+    { review, page = 1, limit = 10, employee } = {},
+    { rejectWithValue }
+  ) => {
     try {
       const queryParams = new URLSearchParams();
-      if (page) queryParams.append('page', page);
-      if (limit) queryParams.append('limit', limit);
-      if (review) queryParams.append('review', review);
+      if (page) queryParams.append("page", page);
+      if (limit) queryParams.append("limit", limit);
+      if (review) queryParams.append("review", review);
+      if (employee) queryParams.append("employee", employee);
 
-      const { data } = await axiosInstance.get(`/feedbacks?${queryParams.toString()}`);
+      const { data } = await axiosInstance.get(
+        `/feedbacks?${queryParams.toString()}`
+      );
       return data;
     } catch (error) {
       return rejectWithValue(
