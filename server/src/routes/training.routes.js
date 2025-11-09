6746cb58612941c508cc07086c0e81a8b241fda7
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2025-11-09 20:10:35 +0500

    feat: add routes for employee-specific training retrieval

--- server/src/routes/training.routes.js
+++ server/src/routes/training.routes.js
@@ -3,13 +3,17 @@ import {
   createTraining,
   getTraining,
   getAllTrainings,
+  getMyTrainings,
+  getTrainingForEmployee,
 } from "../controllers/training.controller.js";
-import { verifyAdminToken } from "../middlewares/index.js";
+import { verifyAdminToken, verifyEmployeeToken } from "../middlewares/index.js";
 
 const router = express.Router();
 
 router.post("/", verifyAdminToken, createTraining);
 router.get("/", verifyAdminToken, getAllTrainings);
+router.get("/my", verifyEmployeeToken, getMyTrainings);
+router.get("/:id/my", verifyEmployeeToken, getTrainingForEmployee);
 router.get("/:id", verifyAdminToken, getTraining);
 
 export default router;
